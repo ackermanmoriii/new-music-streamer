f7c59145fac7f7cb757f3cb233fa8cfd18312be6
ackermanmoriii <ackermanmoriii@gmail.com>
2025-12-03 00:46:44 +0330

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # Install system dependencies
 apt-get update
-apt-get install -y ffmpeg
+apt-get install -y ffmpeg libavcodec-extra
 
 # Install Python dependencies
-pip install -r requirements.txt
+pip install --no-cache-dir -r requirements.txt
+
+# Create necessary directories
+mkdir -p templates
